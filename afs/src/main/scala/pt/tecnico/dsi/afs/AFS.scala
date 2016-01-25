ee875029f6cfdf486e8be77f29558379fccd7a69
Rui Botelho <rui.botelho@tecnico.ulisboa.pt>
2016-01-25 16:22:45 +0000

    forget to add tests

--- afs/src/main/scala/pt/tecnico/dsi/afs/AFS.scala
+++ afs/src/main/scala/pt/tecnico/dsi/afs/AFS.scala
@@ -2,7 +2,10 @@ package pt.tecnico.dsi.afs
 
 import java.io.File
 
+import squants.storage.StorageConversions._
 import squants.storage.{Kilobytes, Storage}
+import squants.storage.{Kilobytes, Storage}
+import work.martins.simon.expect.EndOfFile
 import work.martins.simon.expect.fluent.Expect
 
 import scala.util.matching.Regex.Match
@@ -14,20 +17,6 @@ import work.martins.simon.expect.fluent._
   */
 object AFS {
 
-  def listquota(dir: File): Expect[Either[ErrorCase,(String,Storage,Storage)]] = {
-    val command = "fs listquota -path "+dir.getAbsolutePath
-    val e = new Expect[Either[ErrorCase,(String,Storage,Storage)]](command,Left(UnknownError))
-    e.expect
-      .when(
-        """Volume Name\s+Quota\s+Used\s+%Used\s+Partition
-          |([^\s]+)\s+(\d+)\s+(\d+)""".stripMargin.r)
-      .returning{ m: Match =>
-        Right((m.group(1),Kilobytes(m.group(2).toInt),Kilobytes(m.group(3).toInt)))
-      }
-    e
-  }
-
-
   /*
     Error messages
   */
@@ -38,5 +27,45 @@ object AFS {
   case object InvalidUserName extends ErrorCase
   case object AFSIdAlreadyTaken extends ErrorCase
   case object UnknownError extends ErrorCase
+  case object NotImplemented extends ErrorCase
+
+  def listquota(directory: File): Expect[Either[ErrorCase,(String,Storage,Storage)]] = {
+    val dir = directory.getPath
+    val command = s"fs listquota -path $dir"
+    val e = new Expect[Either[ErrorCase,(String,Storage,Storage)]](command,Left(UnknownError))
+    e.expect
+      .when(s"File '$dir' doesn't exist")
+        .returning(Left(InvalidDirectory))
+      //Note that Quota might be "no limit" but since we do not allow to set a quota to no limit we don't handle this case
+      .when(
+        """Volume Name\s+Quota\s+Used\s+%Used\s+Partition
+          |([^\s]+)\s+(\d+)\s+(\d+)""".stripMargin.r)
+        .returning{ m: Match =>
+          //Quota and Used are in kilobytes
+          Right((m.group(1),Kilobytes(m.group(2).toInt),Kilobytes(m.group(3).toInt)))
+        }
+    e
+  }
+
+  def setQuota(directory: File, quota: Storage): Expect[Either[ErrorCase, Boolean]] = {
+    require(quota > 0.kilobytes, "Quota must be positive")
+    val dir = directory.getPath
+    val command = s"fs setquota -path $dir -max ${quota.toKilobytes.toLong}"
+    val e = new Expect[Either[ErrorCase, Boolean]](command, Left(UnknownError))
+    e.expect
+      .when(s"File '$dir' doesn't exist")
+      .returning(Left(InvalidDirectory))
+      .when(EndOfFile)
+      .returning(Right(true))
+    e
+  }
+
+  def listACL(directory: File): Expect[Either[ErrorCase, Map[String, Permission]]] = {
+    new Expect[Either[ErrorCase, Map[String, Permission]]]("",Left(NotImplemented))
+  }
+
+
+
+
 
 }
--- afs/src/test/scala/pt/tecnico/dsi/AFSSpec.scala
+++ afs/src/test/scala/pt/tecnico/dsi/AFSSpec.scala
@@ -2,8 +2,12 @@ package pt.tecnico.dsi
 
 import java.io.File
 
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{Matchers, FlatSpec}
 import pt.tecnico.dsi.afs.AFS
+import pt.tecnico.dsi.afs.AFS.ErrorCase
+import squants.storage.{Gigabytes, Kilobytes, Storage}
+import work.martins.simon.expect.fluent.Expect
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
@@ -12,11 +16,68 @@ import scala.concurrent.ExecutionContext.Implicits.global
 /**
   *
   */
-class AFSSpec extends FlatSpec with Matchers{
-  "listquota" should "???" in {
-    val dir = "/afs/ist.utl.pt/groups/dsi-panel-tests"
-    val e = AFS.listquota(new File(dir))
-    val ret = Await.result(e.run(),5.minutes)
-    println(s"listquota result:$ret")
+class AFSSpec extends FlatSpec with ScalaFutures with Matchers{
+  val path = "/afs/ist.utl.pt/groups/dsi-panel-tests"
+  val tmpFile = "random.data"
+  val defaultQuota = Kilobytes(2048000)
+  val volumeName: String = "group.dsi-panel-tests"
+  val nonExistantDir = "dirx"
+  /*
+    removes all files and directories in the path afs directory
+   */
+  def clean() = {
+    sys.process.Process(Seq("rm","-f",tmpFile), new java.io.File(path)).!!
+    val command = s"fs setquota -path ${new File(path).getPath} -max ${defaultQuota.toKilobytes.toLong}"
+    sys.process.Process(command.split(" "), new File(path)).!!
+
+  }
+
+  def listquota(path: String): (String, Storage, Storage) = {
+    val e = AFS.listquota(new File(path))
+    e.run().futureValue match {
+      case Right((name, quota, used)) => {
+        (name, quota, used)
+      }
+      case Left(_) => {
+        (nonExistantDir,Kilobytes(0),Kilobytes(0))
+      }
+    }
+  }
+
+  "AFS listquota" should "return (name, quota, used) when the directory exists" in {
+    clean()
+    val (name1,quota1,used1) = listquota(path)
+    assertResult(volumeName)(name1)
+  }
+
+  it should "return error when directory does not exist" in {
+    val (name1,quota1,used1) = listquota(nonExistantDir)
+    assertResult(nonExistantDir)(name1)
+  }
+
+  it should "return updated used value after adding a file" in {
+    clean()
+    val (name1,quota1,used1) = listquota(path)
+    assertResult(volumeName)(name1)
+    // create a file of 1 kilobyte in test afs path
+    sys.process.Process(s"dd if=/dev/zero of=$tmpFile bs=1K count=1", new java.io.File(path)).!!
+    val (name2,quota2,used2) = listquota(path)
+    assertResult(volumeName)(name2)
+    assertResult(quota1)(quota2)
+    assertResult(used1 + Kilobytes(1))(used2)
+  }
+
+
+  "AFS setquota" should "after increasing the quota it should return the same " +
+    "volume name, the new set quota, the same used space" in {
+    val (name1,quota1,used1) = listquota(path)
+
+    val e2 = AFS.setQuota(new File(path),Gigabytes(1))
+    val t2: Either[ErrorCase, Boolean] = Await.result(e2.run(),5.seconds)
+    assertResult(Right(true))(t2)
+    val (name2,quota2,used2) = listquota(path)
+    assertResult(volumeName)(name2)
+    assertResult(Gigabytes(1))(quota2)
+    assertResult(used1)(used2)
   }
 }
